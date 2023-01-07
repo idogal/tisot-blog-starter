53e2173694684ea1703bce3bea6fb54be1e01383
Ido Gal <idogal@outlook.com>
2023-01-07 18:36:11 +0200

    11ty njk template init

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,13 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/assets/img": "assets/img" });
+  eleventyConfig.addPassthroughCopy({ "src/assets/favicon": "assets/favicon" });
+
   return {
+    passthroughFileCopy: true,
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    templateFormats: ["md", "njk", "html", "liquid"],
+    pathPrefix: "",
     dir: {
       input: "src",
       output: "dist",
