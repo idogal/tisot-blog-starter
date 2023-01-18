5fbacc7aaa4391454c03ce1a7332e98245598540
Ido Gal <idogal@outlook.com>
2023-01-18 10:44:21 +0200

    Added tailwind to the build process

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,25 @@
+// const postcss = require('postcss');
+const tailwindcss = require("tailwindcss");
+// const autoprefixer = require('autoprefixer');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets/img": "assets/img" });
   eleventyConfig.addPassthroughCopy({ "src/assets/favicon": "assets/favicon" });
 
+  // eleventyConfig.addNunjucksAsyncFilter('postcss', (cssCode, done) => {
+  //   postcss([tailwindcss(require('./tailwind.config.js')), autoprefixer()])
+  //     .process(cssCode)
+  //     .then(
+  //       (r) => done(null, r.css),
+  //       (e) => done(e, null)
+  //     );
+  // });
+  eleventyConfig.addWatchTarget("styles/**/*.css");
+
   return {
     passthroughFileCopy: true,
     htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
     templateFormats: ["md", "njk", "html", "liquid"],
     pathPrefix: "",
