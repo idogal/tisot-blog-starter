14b72756fb82d9986a0e76f460ec2463a4e6852f
Ido Gal <idogal@outlook.com>
2023-02-12 15:58:49 +0200

    temp 230212

--- src/_data/translations/index.js
+++ src/_data/translations/index.js
@@ -1,46 +1,58 @@
 module.exports = {
-  "About": {
-    "he": "אודות",
-    "en": "About"    
+  About: {
+    he: "אודות",
+    en: "About",
   },
-  "Home": {
-    "he": "דף הבית",
-    "en": "Home"    
+  Home: {
+    he: "דף הבית",
+    en: "Home",
   },
-  "Blog": {
-    "he": "בלוג",
-    "en": "Blog"    
+  Blog: {
+    he: "בלוג",
+    en: "Blog",
   },
-  "Contact": {
-    "he": "צור קשר",
-    "en": "Contact"    
+  Contact: {
+    he: "צור קשר",
+    en: "Contact",
   },
-  "Tisot": {
-    "he": "טיסות",
-    "en": "Tisot"  
+  Tisot: {
+    he: "טיסות",
+    en: "Tisot",
   },
-  "Using": {
-    "he": "באמצעות שימוש ב",
-    "en": "Using"  
+  Using: {
+    he: "באמצעות שימוש ב",
+    en: "Using",
   },
   "Using the": {
-    "he": "באמצעות שימוש ב",
-    "en": "Using the"  
+    he: "באמצעות שימוש ב",
+    en: "Using the",
+  },
+  "Photo by": {
+    he: "תמונה על ידי",
+    en: "Photo by",
+  },
+  Photo: {
+    he: "תמונה",
+    en: "Photo",
+  },
+  by: {
+    he: "על ידי",
+    en: "by",
   },
   "forms endpoint": {
-    "he": "מערכת טפסים",
-    "en": "forms endpoint"  
+    he: "מערכת טפסים",
+    en: "forms endpoint",
   },
-  "and": {
-    "he": "ו",
-    "en": "and"  
+  and: {
+    he: "ו",
+    en: "and",
   },
-  "footer_built_by": {
-    "he": "נבנה על ידי עדוא גל, 2023",
-    "en": "Built by Ido Gal, 2023" 
+  footer_built_by: {
+    he: "נבנה על ידי עדוא גל, 2023",
+    en: "Built by Ido Gal, 2023",
   },
-  "repo_message": {
-    "he": "פרויקט גיטלאב",
-    "en": "GitLab repo" 
-  }    
+  repo_message: {
+    he: "פרויקט גיטלאב",
+    en: "GitLab repo",
+  },
 };
