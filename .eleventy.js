3ae1f4a52fdd07f32492ad1c9ce59aa2ffb30578
Ido Gal <idogal@outlook.com>
2023-02-17 21:11:13 +0200

    Set the photo credit on the about page as shortcode
    2

--- .eleventy.js
+++ .eleventy.js
@@ -30,6 +30,21 @@ module.exports = function (eleventyConfig) {
     defaultLanguage: "he",
   });
 
+  eleventyConfig.addShortcode(
+    "photoCredit",
+    (photoBy, subtitle) =>
+      `<p class="text-xs absolute bottom-0 px-1 py-1 text-neutral-50 bg-slate-500 mix-blend-normal opacity-80 rounded-bl rounded-tr">
+        ${photoBy}
+        <a href="https://unsplash.com/es/@leoand1?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        Andrii Leonov
+        </a>
+        ${subtitle}
+        <a href="https://unsplash.com/photos/P5nZwlbTngI?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        Unsplash
+        </a>
+    </p>`
+  );
+
   // eleventyConfig.addNunjucksAsyncFilter('postcss', (cssCode, done) => {
   //   postcss([tailwindcss(require('./tailwind.config.js')), autoprefixer()])
   //     .process(cssCode)
