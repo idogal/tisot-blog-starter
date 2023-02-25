cc45a03501877efa16c353c767939e82f5fdf74d
Ido Gal <idogal@outlook.com>
2023-02-25 16:08:07 +0200

    descrese 2

--- src/assets/code/js/blog_custom.js
+++ src/assets/code/js/blog_custom.js
@@ -4,10 +4,13 @@ const tailwindTextSizes = {
     { className: "text-sm", sizeRank: 2 },
     { className: "text-base", sizeRank: 3 },
     { className: "text-lg", sizeRank: 4 },
+    { className: "text-xl", sizeRank: 5 },
+    { className: "text-2xl", sizeRank: 6 },
+    { className: "text-3xl", sizeRank: 7 }
   ],
 };
 
-function getTailwindTextSizes() {
+function increaseTailwindTextSize() {
   const sizes = tailwindTextSizes.sizes;
 
   const sizesClassNames = new Set();
@@ -51,13 +54,63 @@ function getTailwindTextSizes() {
   }
 }
 
+function decreaseTailwindTextSize() {
+  const sizes = tailwindTextSizes.sizes;
+
+  const sizesClassNames = new Set();
+  sizes.forEach((currentSize) => sizesClassNames.add(currentSize.className));
+
+  const defaultBodySizeClassDefintion = "text-base";
+  let currentBodySizeClassDefintion = "";
+  let bodySizeClassDefintionForLogic = "";
+  const currentBodyClassString = document.body.className;
+  sizesClassNames.forEach((sizeClassName) => {
+    if (currentBodyClassString.includes(sizeClassName)) {
+      currentBodySizeClassDefintion = sizeClassName;
+    }
+  });
+
+  bodySizeClassDefintionForLogic =
+    currentBodySizeClassDefintion === ""
+      ? defaultBodySizeClassDefintion
+      : currentBodySizeClassDefintion;
+
+  let currentBodySizeClassRank;
+  let nextBodySizeClassDefintion = "";
+  sizes.forEach((currentSize) => {
+    if (currentSize.className == bodySizeClassDefintionForLogic)
+      currentBodySizeClassRank = currentSize.sizeRank;
+  });
+  sizes.forEach((currentSize) => {
+    if (currentSize.sizeRank == currentBodySizeClassRank - 1)
+      nextBodySizeClassDefintion = currentSize.className;
+  });
+
+  if (nextBodySizeClassDefintion === "") {
+    return;
+  }
+
+  if (currentBodySizeClassDefintion === "") {
+    document.body.classList.add(nextBodySizeClassDefintion);
+  } else {
+    document.body.classList.remove(currentBodySizeClassDefintion);
+    document.body.classList.add(nextBodySizeClassDefintion);
+  }
+}
+
 var increaseTextSizeButton = document.getElementById("increasetextsize");
+var decreaseTextSizeButton = document.getElementById("decreasetextsize");
 
 const increaseTextSize = function () {
-  getTailwindTextSizes();
+  increaseTailwindTextSize();
+};
+
+const decreaseTextSize = function () {
+  decreaseTailwindTextSize();
 };
 
 increaseTextSizeButton.addEventListener("click", increaseTextSize);
+decreaseTextSizeButton.addEventListener("click", decreaseTextSize);
 
 
 // var checkbox = document.querySelector("#checkbox");
