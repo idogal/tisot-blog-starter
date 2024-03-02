81ce6458bbe6ccbd3090279b4d19a3619c829d35
Ido Gal <idogal@outlook.com>
2024-03-02 17:50:15 +0200

    Updated series colours to Daisyui 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,38 +6,47 @@ const typographyPlugin = require("@tailwindcss/typography");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,njk,md}"],
-  safelist: [
-    'prose-sm',
-    'prose-base',
-    'prose-lg',
-    'prose-xl',
-    'prose-2xl',
-  ],
+  safelist: ["prose-sm", "prose-base", "prose-lg", "prose-xl", "prose-2xl"],
   theme: {
     fontFamily: {
-      sans: ["Alef", "ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", 
-             "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      sans: [
+        "Alef",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
     },
     extend: {
       typography: {
         DEFAULT: {
           css: {
             div: {
-              "line-height": "1.5rem;"
+              "line-height": "1.5rem;",
             },
-            p: {              
-              "text-align": "justify;"
-            }
+            p: {
+              "text-align": "justify;",
+            },
           },
         },
       },
       strokeWidth: {
-        '1': '1px',
-        '1.5': '1.5px',
-        '2': '2px',
-        '2.5': '2.5px',
-        '3': '3px',
-      }      
+        1: "1px",
+        1.5: "1.5px",
+        2: "2px",
+        2.5: "2.5px",
+        3: "3px",
+      },
     },
   },
   daisyui: {
@@ -45,10 +54,5 @@ module.exports = {
     rtl: true,
     darkTheme: "business",
   },
-  plugins: [
-    daisyuiPlugin,
-    tailwindFlipPlugin,
-    prosePlugin,
-    typographyPlugin
-  ],
+  plugins: [daisyuiPlugin, tailwindFlipPlugin, prosePlugin, typographyPlugin],
 };
