0ba783a3fe4ee4da8004de23f69b196c2a338b5b
Ido Gal <idogal@outlook.com>
2024-03-10 08:05:26 +0200

    Testing the title of the demo site

--- src/test/test-main.js
+++ src/test/test-main.js
@@ -1,43 +1,63 @@
 const { By, Builder, Browser } = require("selenium-webdriver");
-const Chrome = require('selenium-webdriver/chrome');
+const Chrome = require("selenium-webdriver/chrome");
 const assert = require("assert");
+var log4js = require("log4js");
 
+const logger = log4js.getLogger();
+logger.level = "debug";
+
+const MAIN_TEST_URL = "http://ido.g.gitlab.io/idog-blog-daisyui/";
+
+async function getDriver(browser) {
+  const options = new Chrome.Options();
+  logger.info("Trying to get driver of:", browser);
+
+  const driver = await new Builder()
+    .setChromeOptions(
+      options
+        .addArguments("--headless")
+        .addArguments("--no-sandbox")
+        .addArguments("--disable-gpu")
+    )
+    .forBrowser(browser)
+    .build();
+
+  logger.info("Got driver of:", browser);
+
+  logger.info("Trying to read page:", MAIN_TEST_URL);
+  await driver.get(MAIN_TEST_URL);
+  logger.info("Got page:", MAIN_TEST_URL);
+
+  return driver;
+}
 
 (async function firstTest() {
   let driver;
-  const options = new Chrome.Options()
 
   try {
-    console.log("Trying to get driver...");
-
-    driver = await new Builder()
-      .setChromeOptions(options.addArguments('--headless').addArguments('--no-sandbox').addArguments('--disable-gpu'))
-      .forBrowser(Browser.CHROME)
-      .build();
-
-    console.log("Trying to read page");
-    await driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
-    console.log("Got driver for CHROME");
+    driver = await getDriver(Browser.CHROME);
 
     let title = await driver.getTitle();
-    console.log("Got title ", title);
-    assert.equal("Web form", title);
+    logger.debug(`Got title: "${title}"`);
+    const PAGE_TITLE = "Home Page | Tisot - A blog starter kit";
+    assert.equal(PAGE_TITLE, title, `Title of the page is supposed to be: '${PAGE_TITLE}'`);
+
+    /*
 
     await driver.manage().setTimeouts({ implicit: 500 });
-
     let textBox = await driver.findElement(By.name("my-text"));
     let submitButton = await driver.findElement(By.css("button"));
-
     await textBox.sendKeys("Selenium");
     await submitButton.click();
-
     let message = await driver.findElement(By.id("message"));
     let value = await message.getText();
     assert.equal("Received!", value);
+    */
   } catch (e) {
-    console.log(e);
+    logger.error(e);
   } finally {
-    if (driver) await driver.quit();
+    if (driver) {
+      await driver.quit();
+    }
   }
 })();
