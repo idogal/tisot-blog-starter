2d326171fefc0ece2a67ba879d76f6bd46beae08
Ido Gal <idogal@outlook.com>
2024-03-12 08:27:46 +0200

    Tidying up daisyUI's setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,8 +31,7 @@ module.exports = {
       typography: {
         DEFAULT: {
           css: {
-            div: {
-            },
+            div: {},
             p: {
               "text-align": "justify;",
             },
@@ -52,6 +51,12 @@ module.exports = {
     themes: ["dim", "corporate"],
     rtl: true,
     darkTheme: "dim",
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
   },
-  plugins: [daisyuiPlugin, tailwindFlipPlugin, prosePlugin, typographyPlugin],
+  plugins: [tailwindFlipPlugin, typographyPlugin, prosePlugin, daisyuiPlugin],
 };
