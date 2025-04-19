c4380b18833274ec9094ec0c1294d0d830d11ecb
Ido Gal <idogal@outlook.com>
2025-04-19 10:30:00 +0300

    Major upgrade to Tailwindcss 4 and DaisyUI 5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
-const daisyuiPlugin = require("daisyui");
-const tailwindFlipPlugin = require("tailwindcss-flip");
-const prosePlugin = require("./prose-plugin");
+// const prosePlugin = require("./prose-plugin");
 const typographyPlugin = require("@tailwindcss/typography");
 
 /** @type {import('tailwindcss').Config} */
@@ -47,16 +45,5 @@ module.exports = {
       },
     },
   },
-  daisyui: {
-    themes: ["dim", "corporate"],
-    rtl: true,
-    darkTheme: "dim",
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
-  },
-  plugins: [tailwindFlipPlugin, typographyPlugin, prosePlugin, daisyuiPlugin],
+  plugins: [ typographyPlugin ],
 };
